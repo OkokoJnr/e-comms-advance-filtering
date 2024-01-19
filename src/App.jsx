ea6db288c972b51a109ddaf22ddccb99c72e9f74
OKOKO Godwin <godwinjnrokokoorokpo@gmail.com>
2024-01-19 15:18:53 +0100

    set up folder and file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
-
+import Navigation from './Navigation/Nav'
+import Products from './Products/Products'
+import Recommended from './Recommended/Recommended'
+import Category from './SideBar/Category/Category'
+import Colors from './SideBar/Colors/Colors'
+import Price from './SideBar/Price/Price'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello nWorld</h1>
+    <Navigation/>
+    <Products/>
+    <Recommended/>
+    <Category/>
+    <Colors/>
+    <Price/>
+    
     </>
   )
 }
